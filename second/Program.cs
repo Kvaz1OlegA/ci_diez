47ddf0c487ae65dc6898a8c4efe9e2eb9dd54983
Kvaz1Oleg1 <nikita.zn11102001@gmail.com>
2020-10-21 10:14:29 +0300

    laba

---
+++ second/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace second
+{
+    class Program
+    {
+        static ManualResetEvent waitHandle = new ManualResetEvent(false);
+        static void Main(string[] args)
+        {
+            Server server = new Server();
+            server.OnStart();
+            waitHandle.WaitOne();
+        }
+    }
+}
---
+++ second/Server.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace second
+{
+    class Server
+    {
+        private FileSystemWatcher watcher;
+        private string sPath = @"C:\Documents\second\SDir\";
+        private string tPath = @"C:\Documents\second\TDir\";
+
+
+        public void OnStart()
+        {
+            watcher = new FileSystemWatcher(@"C:\Documents\second\SDir\");
+            watcher.NotifyFilter = NotifyFilters.LastAccess
+                                | NotifyFilters.LastWrite
+                                | NotifyFilters.FileName
+                                | NotifyFilters.DirectoryName;
+            watcher.Created += OnCreated;
+            watcher.EnableRaisingEvents = true;
+            
+        }
+
+        private static void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            Archiver.Compress(@"C:\Documents\second\SDir\" + e.Name, @"C:\Documents\second\TDir\" + Path.GetFileNameWithoutExtension(e.Name) + ".gz");
+        }
+    }
+}
